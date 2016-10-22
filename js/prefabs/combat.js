a545fedf93e53099a358e6a91f9b42a6ff89a012
nicholasdrzewiecki <nicholasdrzewiecki@gmail.com>
2016-10-21 21:56:35 -0700

    Removed old files.

--- js/prefabs/combat.js
+++
@@ -1,7 +0,0 @@
-// Combat information will go here so we don't have a massive game.js file
-var CAYUMSQUEST = CAYUMSQUEST || {};
-
-CAYUMSQUEST.Battle = function(game) {
-    // Setting context for arguments
-    this.game = game;
-};
